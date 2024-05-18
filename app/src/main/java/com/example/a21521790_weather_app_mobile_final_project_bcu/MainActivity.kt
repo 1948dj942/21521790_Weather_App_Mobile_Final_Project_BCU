b5b470796695fa238b2975ea2611f80727d5aec3
Pham Phuc Toan <toanpham31803@gmail.com>
2024-05-18 13:53:31 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a21521790_weather_app_mobile_final_project_bcu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    //EditText edtSearch
+    //Button btnSearch, btnChangeActivity
+    //Text txtName, txtCountry, txtTemp, txtState, txtHumidity, txtCloud, txtWind, txtDay
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "21521790_Weather_App_Mobile_Final_Project_BCU"
+include(":app")

55ff80f861c6a27d553f65abd0d6e61041d9b32b
Pham Phuc Toan <toanpham31803@gmail.com>
2024-05-18 14:21:24 +0700

    Initial Commit

--- app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
+++ app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.a21521790_weather_app_mobile_final_project_bcu
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    //Hi I change here!
-    //EditText edtSearch
-    //Button btnSearch, btnChangeActivity
-    //Text txtName, txtCountry, txtTemp, txtState, txtHumidity, txtCloud, txtWind, txtDay
+    private lateinit var editSearch: EditText
+    private lateinit var btnSearch: Button
+    private lateinit var btnChangeActivity: Button
+    private lateinit var txtName: TextView
+    private lateinit var txtCountry: TextView
+    private lateinit var txtTemp: TextView
+    private lateinit var txtState: TextView
+    private lateinit var txtHumidity: TextView
+    private lateinit var txtCloud: TextView
+    private lateinit var txtWind: TextView
+    private lateinit var txtDay: TextView
+    private lateinit var imgIcon: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +33,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        mapping()
     }
+
+    private fun mapping() {
+        editSearch = findViewById(R.id.edittextSearch)
+        btnSearch = findViewById(R.id.buttonSearch)
+        btnChangeActivity = findViewById(R.id.buttonChangeActivity)
+        txtName = findViewById(R.id.textviewName)
+        txtCountry = findViewById(R.id.textviewCountry)
+        txtTemp = findViewById(R.id.textviewTemp)
+        txtState = findViewById(R.id.textviewHumidity)
+        txtHumidity = findViewById(R.id.textviewHumidity)
+        txtCloud = findViewById(R.id.textviewCloud)
+        txtWind = findViewById(R.id.textviewWind)
+        txtDay = findViewById(R.id.textviewDay)
+        imgIcon = findViewById(R.id.imageIcon)
+    }
+
 }

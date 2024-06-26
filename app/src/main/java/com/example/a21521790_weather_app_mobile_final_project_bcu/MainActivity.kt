94d9f85157a5353667f5b233847d0a92c3657adc
Pham Phuc Toan <toanpham31803@gmail.com>
2024-06-26 21:55:49 +0700

    Init commit

--- app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
+++ app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
@@ -1,54 +1,129 @@
 package com.example.a21521790_weather_app_mobile_final_project_bcu
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.w3c.dom.Text
 class MainActivity : AppCompatActivity() {
-    ////
-    private lateinit var editSearch: EditText
-    private lateinit var btnSearch: Button
-    private lateinit var btnChangeActivity: Button
-    private lateinit var txtName: TextView
-    private lateinit var txtCountry: TextView
-    private lateinit var txtTemp: TextView
-    private lateinit var txtState: TextView
-    private lateinit var txtHumidity: TextView
-    private lateinit var txtCloud: TextView
-    private lateinit var txtWind: TextView
-    private lateinit var txtDay: TextView
-    private lateinit var imgIcon: ImageView
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var tvLatitude: TextView
+    private lateinit var tvLongitude: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        tvLatitude = findViewById(R.id.tv_latitude)
+        tvLongitude = findViewById(R.id.tv_longitude)
+
+        getCurrentLocation()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getCurrentLocation() {
+        if(checkPermission())
+        {
+            if(isLocationEnabled()) {
+                //final location here:
+                if(ActivityCompat.checkSelfPermission(
+                        this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(
+                            this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED) {
+                    requestPermission()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task->
+                    val location: Location?=task.result
+                    if(location==null)
+                    {
+                        Toast.makeText(this, "Null Received", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(this, "Get Success", Toast.LENGTH_SHORT).show()
+                        tvLatitude.text ="" +location.latitude
+                        tvLongitude.text=""+location.longitude
+                    }
+
+                }
+            }
+            else {
+                //setting open here
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+                val intent= Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+
+            }
         }
-        mapping()
+        else {
+            //request permission
+            requestPermission()
+        }
     }
 
-    private fun mapping() {
-        editSearch = findViewById(R.id.edittextSearch)
-        btnSearch = findViewById(R.id.buttonSearch)
-        btnChangeActivity = findViewById(R.id.buttonChangeActivity)
-        txtName = findViewById(R.id.textviewName)
-        txtCountry = findViewById(R.id.textviewCountry)
-        txtTemp = findViewById(R.id.textviewTemp)
-        txtState = findViewById(R.id.textviewHumidity)
-        txtHumidity = findViewById(R.id.textviewHumidity)
-        txtCloud = findViewById(R.id.textviewCloud)
-        txtWind = findViewById(R.id.textviewWind)
-        txtDay = findViewById(R.id.textviewDay)
-        imgIcon = findViewById(R.id.imageIcon)
+    private fun isLocationEnabled():Boolean {
+        val locationManager:LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        //checking location is enabled by GPS or Network
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf( android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    companion object{
+        private  const val  PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+    private fun checkPermission(): Boolean
+    {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode== PERMISSION_REQUEST_ACCESS_LOCATION)
+        {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            }
+            else{
+                Toast.makeText(applicationContext, "Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }

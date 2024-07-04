4ee291f1a1ee83fec491630b5caa7435850d62e1
Pham Phuc Toan <toanpham31803@gmail.com>
2024-07-05 00:01:50 +0700

    Try to fixing activity_main.xml com.google.android.material.card.MaterialCardView error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    //
+    implementation("com.google.android.material:material:1.12.0")
     // Add the play-services-location dependency
     implementation(libs.play.services.location)
 
--- app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
+++ app/src/main/java/com/example/a21521790_weather_app_mobile_final_project_bcu/MainActivity.kt
@@ -318,3 +318,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
